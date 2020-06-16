345b46dc0effc6156502279cd4c6feefa7912ac5
chandrasekharareddym <mcsr.in@gmail.com>
2020-06-16 15:23:25 +0530

    committ my changes

--- src/main/java/gitRepo/SelTestProj/App2.java
+++ src/main/java/gitRepo/SelTestProj/App2.java
@@ -9,5 +9,6 @@ public class App2
     public static void main( String[] args )
     {
         System.out.println( "Hello World22222222222    !" );
+        System.out.println( "Hello World22222222222 second   !" );
     }
 }
