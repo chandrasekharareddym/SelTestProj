befb335240ebeee41f7665c5b1134d6776f9aedb
chandrasekharareddym <mcsr.in@gmail.com>
2020-06-16 14:50:53 +0530

    commiting new code file into branch

---
+++ src/main/java/gitRepo/SelTestProj/App2.java
@@ -0,0 +1,13 @@
+package gitRepo.SelTestProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App2 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!222222222222222" );
+    }
+}
